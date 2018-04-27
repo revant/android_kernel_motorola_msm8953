14cf340061edc6a892e16ff2f59a417b0bd91b43
Revant Nandgaonkar <revant.one@gmail.com>
2018-04-27 16:01:25 +0530

    utilities to build kernel

---
+++ envsetup.sh
@@ -0,0 +1,10 @@
+echo "Setting ARCH ..."
+export ARCH=arm64
+echo "Setting SUBARCH ..."
+export SUBARCH=arm64
+echo "Setting CROSS_COMPILE ..."
+export CROSS_COMPILE=/run/media/revant/STORAGE/android/lineage/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+echo "Setting PATH ..."
+export PATH=/run/media/revant/STORAGE/android/lineage/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
+echo "Setting O ..."
+export O=out
